572b50e10baadcd730bcfb15a60bb5c77ee64294
Angel Torres <angeltorres7850@gmail.com>
2019-10-07 01:46:59 -0500

    llamada a ws

--- webpage/src/components/Wastes/index.js
+++ webpage/src/components/Wastes/index.js
@@ -73,6 +73,22 @@ export default function MaterialTableDemo() {
             title="Mermas"
             columns={state.columns}
             data={state.data}
+            data={query =>
+              new Promise((resolve, reject) => {
+                let url = 'https://reqres.in/api/users?'
+                url += 'per_page=' + query.pageSize
+                url += '&page=' + (query.page + 1)
+                fetch(url)
+                  .then(response => response.json())
+                  .then(result => {
+                    resolve({
+                      data: result.data,
+                      page: result.page - 1,
+                      totalCount: result.total,
+                    })
+                  })
+              })
+            }
             icons={tableIcons}
             editable={{
                 onRowAdd: newData =>
