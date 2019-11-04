fbdfa6e06eccbef5f020dbbed83641c3df371933
Joel Sabethai <joel.sabethai@gmail.com>
2019-11-04 08:20:42 -0600

    fix restockAlerts

--- server/querys/alerts.js
+++ server/querys/alerts.js
@@ -1,7 +1,7 @@
 const con = require('../DB/connection')
 
 function restockAlerts (req, res) {
-    con.query("SELECT idSupply AS id, nameSupply AS name, quantitySupply AS quantity, ((minQuantitySupply+(maxQuantitySupply-minQuantitySupply)/2)-quantitySupply) AS expectedQuantity FROM supplies WHERE statusSupply=1", function (err, result) {
+    con.query("SELECT idSupply AS id, nameSupply AS name, quantitySupply AS quantity, ((minQuantitySupply+(maxQuantitySupply-minQuantitySupply)/2)-quantitySupply) AS expectedQuantity FROM supplies WHERE statusSupply=1 AND quantitySupply<minQuantitySupply OR quantitySupply>maxQuantitySupply", function (err, result) {
         if (err) {
             console.log("Error" , err)
             res.status(500).json({err})
