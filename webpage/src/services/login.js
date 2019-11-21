f7bf186fb6284572a42268139098d28675172e4f
Angel Torres <angeltorres7850@gmail.com>
2019-11-21 02:03:28 -0600

    login

---
+++ webpage/src/services/login.js
@@ -0,0 +1,24 @@
+import { SERVER_URL } from "../constants";
+
+export const login = async function (user) {
+    try {
+        console.log(user);
+        const response = await fetch(SERVER_URL +`logIn`,{
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
+        console.log(json);
+
+        return json;
+    } catch (error) {
+        console.log(error);
+        return []
+    }
+}
