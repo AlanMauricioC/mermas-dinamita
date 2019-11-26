3a0476ad69471b1cdbfbb5fb6af5c5a590557cca
Diana Edda <dianaedda97@gmail.com>
2019-11-26 00:44:06 -0600

    Fix: alerts and insertSupply

--- DDL/inserts.sql
+++ DDL/inserts.sql
@@ -2,22 +2,22 @@
 INSERT INTO units VALUES (NULL, 'litros', 1), (NULL, 'mililitros', 1);
 
 -- usuarios
-INSERT INTO users VALUES (NULL, 'diana@vivall.com', 'diana', 1, '1234', 1);
+INSERT INTO users VALUES (NULL, 'admi@vivall.com', SHA1('Aa123456_'), 1, '12345', 1);
 
 -- proveedores
-INSERT INTO providers VALUES (NULL, 'lala', 1);
+INSERT INTO providers VALUES (NULL, 'Inventario', 0), (NULL, 'lala', 1);
 
 -- insumos
 INSERT INTO supplies (idSupply, nameSupply, minQuantitySupply, maxQuantitySupply, idUser, idUnit, statusSupply) VALUES (NULL, 'Leche', 3, 8, 1, 1, 1), (NULL, 'chocolate', 3, 8, 1, 1, 1), (NULL, 'chocomilk', 3, 8, 1, 1, 1);
 
 -- recetas
-INSERT INTO recipes VALUES (NULL, 'chocomilk', 'recipes/default.jpg', 'sdfghj', 3, 1.5, 1);
+INSERT INTO recipes VALUES (NULL, 'chocomilk', 'recipes/default.jpg', 'sdfghj', 3, 1.5, 1, 1);
 
 -- insumo de la receta
 INSERT INTO recipesupply VALUES (1, 1, 2), (1, 2, 2);
 
 -- restock
-INSERT INTO restock VALUES (NULL, current_timestamp(), 1, 1);
+INSERT INTO restock VALUES (NULL, current_timestamp(), 1, 7), (NULL, current_timestamp(), 1, 1);
 
 -- insumo de restock
-INSERT INTO restocksupply VALUES (1, 1, 5, 21, NULL, NULL, 1, 1, 'asdfghjkl');
+INSERT INTO restocksupply VALUES (2, 1, 5, 21, current_timestamp(), current_timestamp(), 1, 1, 'asdfghjkl');
